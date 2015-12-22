f5946c2fa074327f7404a988325f354aece1c868
Vinay Bhat <vinaybhat001@gmail.com>
2015-12-22 12:15:28 +0530

    Changes to graydist

--- opencv_graydist.cpp
+++ opencv_graydist.cpp
@@ -21,6 +21,9 @@ extern "C"
   #include "sciprint.h"
   #include "../common.h"
 
+  Point minDistance(Mat, Mat);
+  vector<Point> getNeighbours(Point);
+
   int opencv_graydist(char *fname, unsigned long fname_len)
   {
 
@@ -54,21 +57,8 @@ extern "C"
 
 
     vector<Point> sources;
-    Mat distances(mask.size(), CV_32F);
+    Mat distances = Mat(mask.size(), CV_32F);
     Mat sptSet = Mat::zeros(mask.size(), CV_8UC1);
-    for (i = 0; i < mask.cols; i++)
-    {
-      for (j = 0; j < mask.rows; j++)
-      {
-        if (mask.at<uchar>(i,j)) {
-          sources.push_back(Point(i,j));
-          distances.at<float>(i,j) = 0.0;
-        }
-        else
-          distances.at<float>(i,j) = FLT_MAX;
-      }
-    }
-
     for (i = 0; i < mask.cols; i++)
     {
       for (j = 0; j < mask.rows; j++)
@@ -84,16 +74,27 @@ extern "C"
 
     for (c = 0; c < mask.cols * mask.rows; c++)
     {
-      float l = minDistance(distances, sptSet);
+      Point p = minDistance(distances, sptSet);
+      sptSet.at<uchar>(p.x, p.y) = 1;
+      vector<Point> neighbours = getNeighbours(p);
+      markerVal = marker.at<uchar>(p.x, p.y);
+      for (k = 0; k < neighbours.length(); k++)
+      { 
+        nVal = marker.at<uchar>(neighbours[k].x, neighbours[k].y);
+        distN = (markerVal + nVal) / 2;
+        if (!(sptSet.at<uchar>(neighbours[i].x, neighbours[i].y)) && distances.at<float>(p.x, p.y) != FLT_MAX
+            && (distances.at<float>(p.x, p.y) + distN < distances.at<float>(neighbours[i].x, neighbours[i].y)))
+          distances.at<float>(neighbours[i].x, neighbours[i].y) = distances.at<float>(p.x, p.y) + distN;
+      } 
 
     }
 
 
-    string tempstring = type2str(fin_image.type());
+    string tempstring = type2str(distances.type());
     char *checker;
     checker = (char *)malloc(tempstring.size() + 1);
     memcpy(checker, tempstring.c_str(), tempstring.size() + 1);
-    returnImage(checker, fin_image, 1);
+    returnImage(checker, distances, 1);
     free(checker);
 
     //Assigning the list as the Output Variable
@@ -102,6 +103,36 @@ extern "C"
     ReturnArguments(pvApiCtx);
     return 0;
 
+  }
+
+  Point minDistance(Mat distances, Mat sptSet)
+  {
+    float min = FLT_MIN;
+    Point p;
+    for (i = 0; i < distances.cols; i++)
+    {
+      for (j = 0; j < distances.rows; j++)
+      {
+        if (distances.at<float>(i,j) < min && sptSet.at<uchar>(i,j) == 0)
+        {  
+          min = distances.at<float>(i,j);
+          p = Point(i,j);
+        }
+      }
+    }
+    return p;
+  }
+
+  vector<Point> getNeighbours(Point p)
+  {
+    vector<Point> neighs;
+    Rect rect(Point(), marker.size());
+    for (i = -1; i <= 1; i++) {
+      for (j = -1; j <= 1; j++) {
+        if (rect.contains(Point(p.x + i, p.y + j)))
+          neighs.push_back(Point(p.x + i, p.y + j));
+      }
+    }
   }
 /* ==================================================================== */
 }
